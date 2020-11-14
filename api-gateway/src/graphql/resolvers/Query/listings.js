62c6a1493955d9eaf2163044d0484cdb2ff5452c
rohit goudagaon <rohitsgoudagaon@gmail.com>
2020-11-14 19:34:59 +0530

    added api-gateway setup, did yarn add @babel/core @babel/preset-env @babel/polyfill babel-plugin-module-resolver apollo-server apollo-server-express cookie-parser cors express graphql

---
+++ api-gateway/src/graphql/resolvers/Query/listings.js
@@ -0,0 +1,9 @@
+const listingResolvers = async() => {
+    return [{
+        id: "1",
+        description: 'desc 1',
+        title: 'title 1'
+    }]
+}
+
+export default listingResolvers;
---
+++ api-gateway/src/graphql/typeDefs.js
@@ -0,0 +1,16 @@
+import { gql } from 'apollo-server';
+
+
+const typeDefs = gql `
+  type Listing {
+    id: ID!
+    description: String!
+    title: String!
+  }
+
+  type Query {
+    listings: [Listing!]!
+  }
+`;
+
+export default typeDefs;
